3551db4aa3789820ca13b75c36e6dd10707dc7fd
Manuel vera <vera.manuel2004@gmail.com>
2024-01-16 15:36:26 -0600

    Update useGamesData interface to align with API response structure

--- src/hooks/useGamesData.ts
+++ src/hooks/useGamesData.ts
@@ -1,24 +1,33 @@
 import ApiService from '../services/api-service';
 
 
+interface PlatformDetail {
+    id: number;
+    name: string;
+    slug: string;
+    // Include other relevant fields if needed
+}
+
 interface Platform {
-    platform: {
-        id: number;
-        name: string;
-    }
+    platform: PlatformDetail;
+    // Include other relevant fields if needed
 }
 
 interface Game {
     id: number;
+    slug: string;
     name: string;
     released: string;
     background_image: string;
     metacritic: number;
     platforms: Platform[];
+    // Add other fields from the API response as needed
 }
 
 interface GamesApiResponse {
     count: number;
+    next: string | null;
+    previous: string | null;
     results: Game[];
 }
 
