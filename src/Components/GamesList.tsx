e6a16ddbe6b293f9df2014b6d67d79f7d7f78d56
Manuel vera <vera.manuel2004@gmail.com>
2024-02-04 22:40:19 -0600

    feat: Implement Infinite Scrolling and Enhance UX
    This commit introduces infinite scrolling to the GamesList component using the react-infinite-scroll-component package, significantly improving the user experience by seamlessly loading additional games as the user scrolls. Major highlights include:
    
    - Integration of infinite scrolling in GamesList.tsx, allowing users to effortlessly access more games without manual pagination.
    - Optimization of the useGamesData hook for efficient data fetching and management, ensuring compatibility with infinite scrolling.
    - Resolution of infinite re-render issues by refining the useCachedSortingFiltering hook, enhancing application performance and stability.
    - Comprehensive documentation updates for GamesList.tsx, useGamesData.ts, and useCachedSortingFiltering.ts, providing clear insights into component functionality, usage, and implementation details.
    - Minor bug fixes and code optimizations to improve readability and maintainability.
    
    These enhancements contribute to a more dynamic, responsive, and user-friendly application, encouraging deeper engagement and exploration of the game library.

--- src/Components/GamesList.tsx
+++ src/Components/GamesList.tsx
@@ -1,37 +1,59 @@
 /**
- * GamesList Component
+ * GamesList Component Documentation
  *
- * This component displays a list of video games based on the selected genre, platform, sort option, and search query.
- * It utilizes the `useGamesData` hook to fetch the list of games from an API and the `useCachedSortingFiltering` hook
- * to apply client-side filtering and sorting based on the user's selections. The component is designed to be responsive,
- * adjusting its layout based on the current breakpoint. Additionally, it provides a "Load More" button to fetch additional
- * games if available.
+ * Overview:
+ * The GamesList component renders a responsive list of video games, incorporating features such as filtering, sorting,
+ * and infinite scrolling. It leverages `useGamesData` for fetching game data from an API and `useCachedSortingFiltering`
+ * for client-side processing based on user inputs. The inclusion of the `react-infinite-scroll-component` allows for
+ * efficient loading of additional games as the user scrolls, enhancing the browsing experience.
+ *
+ * Features:
+ * - Responsive design that adapts to various screen sizes and breakpoints.
+ * - Dynamic filtering and sorting based on user selections for genre, platform, and other criteria.
+ * - Infinite scrolling to seamlessly load more games as the user scrolls down, with a "Load More" button as a fallback.
+ * - Skeleton loaders to indicate loading states, providing a smooth user experience.
  *
  * Props:
- * - selectedGenre: The genre selected by the user for filtering the games list.
- * - selectedPlatform: The platform selected by the user for filtering the games list.
- * - sortOption: The option selected by the user for sorting the games list.
- * - searchQuery: The query entered by the user for searching within the games list.
+ * - selectedGenre: Filter games by a specific genre.
+ * - selectedPlatform: Filter games by a specific platform.
+ * - sortOption: Sort the games list according to criteria such as release date or score.
+ * - searchQuery: Filter games by matching text query.
  *
- * Usage:
+ * Usage Example:
+ * ```jsx
  * <GamesList
  *   selectedGenre="Action"
  *   selectedPlatform="PC"
  *   sortOption="Newest"
- *   searchQuery=""
+ *   searchQuery="witcher"
  * />
+ * ```
  *
- * The component structure includes a header displaying the current category, a flex container for the game cards,
- * and conditionally rendered content based on the loading state, presence of an error, or the filtered games list.
+ * Implementation Details:
+ * - The component first fetches a paginated list of games using `useGamesData`. This hook is configured to support
+ *   infinite query behavior, fetching additional pages of data as needed.
+ * - The `useCachedSortingFiltering` hook is applied to the fetched data for client-side filtering and sorting, based
+ *   on the props provided by the user.
+ * - The `react-infinite-scroll-component` is utilized to wrap the list of game cards, triggering additional data fetches
+ *   from `useGamesData` when the user scrolls near the bottom of the list.
+ * - A "Load More" button is also provided as an alternative method for triggering the fetch of additional games.
+ * - Skeleton loaders are displayed during data fetching to indicate loading states, enhancing the visual feedback for
+ *   users.
+ *
+ * Note:
+ * This component plays a crucial role in the application's user interface, presenting a dynamic and interactive list of
+ * games that responds to user inputs and scrolling behavior.
  */
 
 
 
-import { Text, Box, Flex, useBreakpointValue, Button} from '@chakra-ui/react';
+
+import { Text, Box, Flex, useBreakpointValue} from '@chakra-ui/react';
 import GamesCard from "./GameCard.tsx";
 import SkeletonGameCard from './SkeletonGameCard';
 import useCachedSortingFiltering from "../hooks/useCachedSortingFiltering.ts";
 import useGamesData from "../hooks/useGamesData.ts";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 
@@ -57,36 +79,40 @@ function GamesList({ selectedGenre, selectedPlatform, sortOption, searchQuery }:
             <Text fontSize="4xl" as="b" mb={4}>
                 Games
             </Text>
-            <Flex wrap="wrap" justify="space-around" gap={6}>
-                {isLoading ? (
-                    Array(20).fill(null).map((_, index) => <SkeletonGameCard key={index} />)
-                ) : error ? (
-                    <Text fontSize="lg" color="gray.500" marginTop={10}>
-                        Error on request: {error?.message}
-                    </Text>
-                ) : filteredGames && filteredGames.length > 0 ? (
-                    filteredGames.map((game) => (
-                        <GamesCard
-                            key={game.id}
-                            game_name={game.name}
-                            img_url={game.background_image}
-                            score={game.metacritic}
-                            platform={game.platforms}
-                        />
-                    ))
-                ) : (
-                    <Text fontSize="lg" color="gray.500" marginTop={10}>
-                        No games found for the genre '{selectedGenre}'.
-                    </Text>
-                )}
-            </Flex>
-            {hasNextPage && (
-                <Flex justifyContent='center' marginTop='4'>
-                    <Button onClick={() => fetchNextPage()} isLoading={isLoading}>
-                        Load More
-                    </Button>
+            <InfiniteScroll
+                dataLength={filteredGames.length}
+                next={fetchNextPage}
+                hasMore={hasNextPage}
+                loader={<SkeletonGameCard />}
+                endMessage={
+                <p style={{textAlign: 'center'}}>
+                    <b>Yay! You have seen it all</b>
+                </p>
+            }>
+                <Flex wrap="wrap" justify="space-around" gap={6}>
+                    {isLoading ? (
+                        Array(20).fill(null).map((_, index) => <SkeletonGameCard key={index} />)
+                    ) : error ? (
+                        <Text fontSize="lg" color="gray.500" marginTop={10}>
+                            Error on request: {error?.message}
+                        </Text>
+                    ) : filteredGames && filteredGames.length > 0 ? (
+                        filteredGames.map((game) => (
+                            <GamesCard
+                                key={game.id}
+                                game_name={game.name}
+                                img_url={game.background_image}
+                                score={game.metacritic}
+                                platform={game.platforms}
+                            />
+                        ))
+                    ) : (
+                        <Text fontSize="lg" color="gray.500" marginTop={10}>
+                            No games found for the genre '{selectedGenre}'.
+                        </Text>
+                    )}
                 </Flex>
-            )}
+            </InfiniteScroll>
         </Box>
     );
 }
